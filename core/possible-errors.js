1da22fb190311de2360090b3a1b6e1a035614cb0
Mike Geldard <mike.geldard@degree53.com>
2015-11-30 11:48:01 +0000

    Initial commit. Added config.

---
+++ core/possible-errors.js
@@ -0,0 +1,34 @@
+module.exports = {
+
+    "rules": {
+        "comma-dangle": [ 2, "never" ],
+        "no-cond-assign": 0, // review
+        "no-console": 0, // review
+        "no-constant-condition": 0, // review
+        "no-control-regex": 2,
+        "no-debugger": 2,
+        "no-dupe-args": 2,
+        "no-dupe-keys": 2,
+        "no-duplicate-case": 2,
+        "no-empty-character-class": 2,
+        "no-empty": 2,
+        "no-ex-assign": 0, // review
+        "no-extra-boolean-cast": 0, // review
+        "no-extra-parens": 0, // review
+        "no-extra-semi": 2,
+        "no-func-assign": 2,
+        "no-inner-declarations": [ 2, "both" ],
+        "no-invalid-regexp": 2,
+        "no-irregular-whitespace": 2,
+        "no-negated-in-lhs": 0, // review
+        "no-obj-calls": 2,
+        "no-regex-spaces": 2,
+        "no-sparse-arrays": 2,
+        "no-unexpected-multiline": 2,
+        "no-unreachable": 2,
+        "use-isnan": 2,
+        "valid-jsdoc": 0, // review
+        "valid-typeof": 2
+    }
+
+};
---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    "extends": [
+        "./core/best-practices.js",
+        "./core/possible-errors.js",
+        "./core/stylistic-issues.js",
+        "./core/variables.js"
+    ],
+
+    "rules": {
+        "strict": [ 2, "global" ]
+    }
+
+};
