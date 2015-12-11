43f43c7cf8affd8a328c396bc2b07ad96b3b1c66
Mike Geldard <info@mikegeldard.com>
2015-12-11 12:13:25 +0000

    Update possible-errors.js

--- config/possible-errors.js
+++ config/possible-errors.js
@@ -2,9 +2,9 @@ module.exports = {
 
 	"rules": {
 		"comma-dangle": [ 2, "never" ],
-		"no-cond-assign": 0, // review
-		"no-console": 0, // review
-		"no-constant-condition": 0, // review
+		"no-cond-assign": 2,
+		"no-console": 0,
+		"no-constant-condition": 2,
 		"no-control-regex": 2,
 		"no-debugger": 2,
 		"no-dupe-args": 2,
@@ -12,22 +12,27 @@ module.exports = {
 		"no-duplicate-case": 2,
 		"no-empty-character-class": 2,
 		"no-empty": 2,
-		"no-ex-assign": 0, // review
-		"no-extra-boolean-cast": 0, // review
-		"no-extra-parens": 0, // review
+		"no-ex-assign": 2,
+		"no-extra-boolean-cast": 2,
+		"no-extra-parens": 0,
 		"no-extra-semi": 2,
 		"no-func-assign": 2,
 		"no-inner-declarations": [ 2, "both" ],
 		"no-invalid-regexp": 2,
 		"no-irregular-whitespace": 2,
-		"no-negated-in-lhs": 0, // review
+		"no-negated-in-lhs": 2,
 		"no-obj-calls": 2,
 		"no-regex-spaces": 2,
 		"no-sparse-arrays": 2,
 		"no-unexpected-multiline": 2,
 		"no-unreachable": 2,
 		"use-isnan": 2,
-		"valid-jsdoc": 0, // review
+		"valid-jsdoc": [ 2 {
+			"requireReturn": false,
+			"requireParamDescription": true,
+			"requireReturnType": true,
+			"requireReturnDescription": true
+		}],
 		"valid-typeof": 2
 	}
 
